0f8f66e3c4f549d3f3b6a0ab0aa76113f1674266
Kidus Tilahun <kidustilahun@Kiduss-MBP.fios-router.home>
2022-08-03 15:54:47 -0400

    added new education.html file and implemented the onclick function for the nav bar in index.js for index.html and education.html

---
+++ index.js
@@ -0,0 +1,15 @@
+let home = document.getElementById('home-btn');
+let education = document.getElementById('education-btn');
+let skillsprojects = document.getElementById('skill&projects-btn');
+let contacts = document.getElementById('contacts-btn');
+
+
+
+education.onclick = function(){
+    window.open("education.html","_self");
+};
+
+home.onclick = function(){
+    window.open("index.html","_self");
+};
+
